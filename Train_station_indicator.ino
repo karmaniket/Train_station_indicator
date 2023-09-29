88cf707c2225644aa28d1d57964941b178ff19cc
Aniket Vishwakarma <121457666+karmaniket@users.noreply.github.com>
2023-09-30 00:28:51 +0530

    Add files via upload

---
+++ Train_station_indicator.ino
@@ -0,0 +1,45 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+String reminder[] = {
+  "                Next station > Mulund > Destination > Chhatrapati Shivaji Maharaj Terminus (CSMT) ",
+  "                Next station > Ghatkopar > Destination > Chhatrapati Shivaji Maharaj Terminus (CSMT) ",
+  "                Next station > Kurla > Destination > Chhatrapati Shivaji Maharaj Terminus (CSMT) ",
+  "                Next station > Dadar > Destination > Chhatrapati Shivaji Maharaj Terminus (CSMT) ",
+  "                Next station > Byculla > Destination > Chhatrapati Shivaji Maharaj Terminus (CSMT) ",
+  "                Next station > CSMT > Destination > Chhatrapati Shivaji Maharaj Terminus (CSMT) "
+};
+
+String stations[] = {
+  "     Mulund     ",
+  "    Ghatkopar   ",
+  "      Kurla     ",
+  "      Dadar     ",
+  "     Byculla    ",
+  "      CSMT      "
+};
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+}
+
+void loop() {
+  for (int i = 0; i < 6; i++) {
+    String rem = reminder[i];
+    int len = rem.length();
+
+    for (int j = 0; j < len + 16; j++) {d:\Project\Arduino\Train_station_indicator\Train_station_indicator.ino
+      lcd.clear();
+      lcd.print(rem.substring(j, j + 16));
+      delay(70);
+    }
+    lcd.setCursor(0, 1);
+    lcd.print(stations[i]);
+    delay(600);
+    lcd.clear();
+    delay(1000);
+  }
+}
